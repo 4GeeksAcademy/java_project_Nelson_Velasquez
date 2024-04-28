e3f7fe6d471d926513749c62cea21f0e2e5902ea
NelsonAVN <119307206+NelsonAVN@users.noreply.github.com>
2024-04-28 23:16:20 +0000

    mi primer commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excuse();
 };
+function excuse() {
+  let who = [
+    "The dog",
+    "My grandma",
+    "The mailman",
+    "My bird",
+    "My pencil",
+    "The president"
+  ];
+  let action = ["ate", "peed", "crushed", "broke", "destroyed", "stole"];
+  let what = ["my homework", "my phone", "the car", "my heart", "my flipflops"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying",
+    "after I got home",
+    "when my mothe was preparing dinner"
+  ];
+  let who1 = Math.floor(Math.random() * who.length);
+  let action1 = Math.floor(Math.random() * action.length);
+  let what1 = Math.floor(Math.random() * what.length);
+  let when1 = Math.floor(Math.random() * when.length);
+
+  return `${who[who1]} ${action[action1]} ${what[what1]} ${when[when1]}`;
+}
